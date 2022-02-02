c6e29a752a5c903b3d221b9c5a6213110a4b6506
Lzadhito <Lzadhito@gmail.com>
2022-02-03 05:46:00 +0700

    fix(nextjs-youtube-lite): remove multiple .git files

---
+++ apps/nextjs-youtube-lite/pages/index.js
@@ -0,0 +1,10 @@
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
+import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
+
+export default function Home() {
+  return (
+    <div>
+      <LiteYouTubeEmbed id="S7j0ZtXq7Go" />
+    </div>
+  );
+}
