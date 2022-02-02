eaed315d5c44180922c6925a155fc97d092bb80e
Lzadhito <Lzadhito@gmail.com>
2022-02-02 22:50:00 +0700

    youtube-lite

---
+++ apps/svelte-youtube-lite/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<lite-youtube
+	videoid="S7j0ZtXq7Go"
+	style="background-image: url('https://i.ytimg.com/vi/S7j0ZtXq7Go/hqdefault.jpg');"
+	params="rel=0&enablejsapi=1&modestbranding=1&autoplay=1"
+/>
