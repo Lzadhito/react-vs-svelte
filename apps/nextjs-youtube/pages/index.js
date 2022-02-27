70198645a3116fdbe2a469a94465fcad4d378dd7
Lzadhito <Lzadhito@gmail.com>
2022-02-28 06:27:50 +0700

    feat(next-js-youtube): init nextjs youtube non lite

---
+++ apps/nextjs-youtube/pages/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+
+const Home = () => (
+  <ReactPlayer url="https://www.youtube.com/watch?v=2g811Eo7K8U" />
+);
+
+export default Home;
