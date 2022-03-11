5de353e9280ef5b596a85caf4442df78b232b03a
Lzadhito <Lzadhito@gmail.com>
2022-03-11 21:31:42 +0700

    feat(svelte-youtube-lite): update init

--- apps/svelte-youtube-lite/src/routes/index.svelte
+++ apps/svelte-youtube-lite/src/routes/index.svelte
@@ -1,14 +1,10 @@
 <script>
 	import { onMount } from 'svelte';
 
-	onMount(async () => {
-		await import('lite-youtube-embed/src/lite-yt-embed.css');
-		await import('lite-youtube-embed/src/lite-yt-embed.js');
+	onMount(() => {
+		import('lite-youtube-embed/src/lite-yt-embed.js');
+		import('lite-youtube-embed/src/lite-yt-embed.css');
 	});
 </script>
 
-<lite-youtube
-	videoid="S7j0ZtXq7Go"
-	style="background-image: url('https://i.ytimg.com/vi/S7j0ZtXq7Go/hqdefault.jpg');"
-	params="rel=0&enablejsapi=1&modestbranding=1&autoplay=1"
-/>
+<lite-youtube videoid="S7j0ZtXq7Go" />
